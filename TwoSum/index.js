998500863c331a059484b4a8a89808cddfcfa9c9
Sidney François <sidney.fvm.dev@outlook.com>
2023-06-16 16:37:29 -0300

    add jest config

--- TwoSum/index.js
+++ TwoSum/index.js
@@ -2,7 +2,6 @@ export function twoSum (nums, target) {
   let mapper = new Map()
   
   for (let i = 0; i < nums.length; i++) {
-    console.log(typeof i)
     let difference = target - nums[i]
     
     if (mapper.has(difference)) {
